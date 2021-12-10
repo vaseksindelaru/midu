2c4ca7e646e6fa72bb9739b50852944f988313f6
vaclav sindelar <apple@vaclavs-MacBook-Pro.local>
2021-12-10 20:56:37 +0100

    formu addNote-useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import {Note} from "./Note.js";
+import {useState} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default function App (props){
+
+const[xnote,setxnote]= useState(props.notes);
+const [newnote,setnewnote]=useState('')
+
+const handelChange = (event) => {
+setnewnote (event.target.value)}
+
+const handelClick = (event) => {
+  console.log('crear nota')
+  const notetoadd =
+{ 
+  id: xnote.length + 1,
+  content: newnote ,
+  date: new Date().toISOString(),
+  important: Math.random() < 0.5
+}
+console.log(notetoadd)
+
+setxnote(xnote.concat(notetoadd))
+setnewnote('')}
+  return(
+  <div>
+    <ol>
+      {xnote.map((note)=>{
+        return(
+          <Note key ={note.id} {...note} />
+        )
+      })}
+    </ol>
+    <form>
+      <input type='text' onChange={handelChange} value={newnote} ></input>
+      <button onClick={handelClick}>crear notas</button>
+    </form>
+  </div>
+  )
 }
 
-export default App;
+  
+
+
+
---
+++ src/Note.js
@@ -0,0 +1,9 @@
+export const Note = ({id,content,date}) => {
+    console.log(id)
+    return(
+    <li>
+  <p>{content}</p>
+    </li>
+    )}
+
+ 
